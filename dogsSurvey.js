55e97b1d1223d242b9534f09d2905809e2002b16
Chijioke <wisestcodes@gmail.com>
2023-06-27 23:55:12 -0400

    chain methods

--- dogsSurvey.js
+++ dogsSurvey.js
@@ -17,26 +17,24 @@ const kateDogs = [4, 1, 15, 8, 3];
 
 checkDogs(juliaDogs, kateDogs);
 
-const calcAverageHumanAge = dogsAges => {
-  const dogsHumanAges = dogsAges.map(dogAge => {
-    return dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4;
-  });
-  console.log(dogsHumanAges);
+const totalDogHumanAge = dogsAges => {
+  return dogsAges
+    .map(dogAge => {
+      return dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4;
+    })
+    .filter(dogAgeHuman => {
+      return dogAgeHuman >= 18;
+    })
+    .reduce((accumulator, current, i, arr) => {
+      return accumulator + current / arr.length;
+    }, 0);
 
-  const adultDogs = dogsHumanAges.filter(dogAgeHuman => {
-    return !(dogAgeHuman < 18);
-  });
-  console.log(adultDogs);
-
-  const totalDogHumanAge = adultDogs.reduce((accumulator, current) => {
-    return accumulator + current;
-  });
-
-  const averageDogHumanAge = totalDogHumanAge / adultDogs.length;
-  console.log(averageDogHumanAge);
+  // const averageDogHumanAge = totalDogHumanAge / adultDogs.length;
+  // console.log(averageDogHumanAge);
 };
 
 const data1 = [5, 2, 4, 1, 15, 8, 3];
 const data2 = [16, 6, 10, 5, 6, 1, 4];
 
-calcAverageHumanAge(data1);
+const avg1 = totalDogHumanAge(data1);
+console.log(avg1);
